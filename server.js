4e8155525c71acbd63a59708aa1542a540da5de9
Joshua Johnson <joshua_johnson@choicehotels.com>
2015-05-15 16:53:53 -0700

    Initial gulp-angular code

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.logger('dev'));
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
