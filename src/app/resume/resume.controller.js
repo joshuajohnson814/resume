e22644eb4c981c42660982b8d6d3df9e3501d37c
Joshua Johnson <joshua_johnson@choicehotels.com>
2015-05-17 22:34:11 -0700

    Finish building out menu with links to 5 place holder pages About me page summary and interests with link to linked

--- src/app/resume/resume.controller.js
+++ src/app/resume/resume.controller.js
@@ -3,13 +3,19 @@
 
   angular.module('app.resume').controller('Resume', Resume);
 
-  function Resume($mdSidenav) {
+  function Resume($mdSidenav, $state) {
     var vm = this;
 
     vm.toggleNav = toggleNav;
+    vm.goToNav = goToNav;
 
     function toggleNav() {
       $mdSidenav('left').toggle();
+    }
+
+    function goToNav(state) {
+      $state.go(state);
+      vm.toggleNav();
     }
   }
 })();
--- src/app/resume/resume.module.js
+++ src/app/resume/resume.module.js
@@ -15,7 +15,27 @@
       .state('resume.about', {
         url: '/about',
         title: 'About Me: Joshua Johnson',
-        templateUrl: 'app/resume/about/about.html'
+        templateUrl: 'app/resume/pages/about.html'
+      })
+      .state('resume.skills', {
+        url: '/skills',
+        title: 'Skills: Joshua Johnson',
+        templateUrl: 'app/resume/pages/skills.html'
+      })
+      .state('resume.experience', {
+        url: '/experience',
+        title: 'Experience: Joshua Johnson',
+        templateUrl: 'app/resume/pages/experience.html'
+      })
+      .state('resume.education', {
+        url: '/education',
+        title: 'Education: Joshua Johnson',
+        templateUrl: 'app/resume/pages/education.html'
+      })
+      .state('resume.contact', {
+        url: '/contact',
+        title: 'Contact: Joshua Johnson',
+        templateUrl: 'app/resume/pages/contact.html'
       });
   }
 
